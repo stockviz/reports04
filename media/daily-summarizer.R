4f5e65f2c84bdfb34f0358f41cf47b9a84085aca
Shyam Sunder <shyam.sunder@stockviz.biz>
2026-01-01 13:46:09 +0530

    media daily

---
+++ media/daily-summarizer.R
@@ -0,0 +1,18 @@
+library('rmarkdown')
+
+args <- commandArgs(trailingOnly = TRUE)
+if(!is.na(args[1])) {
+    asof <- as.Date(args[1])
+} else {
+    asof <- Sys.Date()
+}
+
+print(asof)
+
+tryCatch({
+	render("daily-summarizer.Rmd", 
+				output_file=paste0("daily/", asof, ".html"), 
+				params=list(asof = asof))
+}, error=function(e){print(e)})
+
+
